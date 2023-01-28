1964f40bf9cb425e0e1d7a57fee1c4f30dab29a0
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-01-27 23:57:10 -0500

    Initial Commit

---
+++ DoULikeMe/DoULikeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoULikeMeApp.swift
+//  DoULikeMe
+//
+//  Created by Vivian Phung on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct DoULikeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
